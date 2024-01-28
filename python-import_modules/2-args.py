f07949eac1881ce88ab07da7e6952755f13bd35f
root <muradabbaszade6@gmail.com>
2024-01-28 06:06:45 -0800

    update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
+if __name__ == "__main__":
     import sys
-    args = sys.argv
-    if len(args) - 1 == 1:
-        print("{} argument.".format(len(args) - 1))
-    elif len(args) - 1 == 0:
-        print("{} arguments:".format(len(args) - 1))
+    argv = sys.argv
+    if len(argv) - 1 == 0:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) - 1 == 1:
+        print("{} argument:".format(len(argv) - 1))
     else:
-        print("{} arguments:".format(len(args) - 1))
-    for i in range(1, len(args)):
-        print("{}: {}".format(i, args[i]))
+        print("{} arguments:".format(len(argv) - 1))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
