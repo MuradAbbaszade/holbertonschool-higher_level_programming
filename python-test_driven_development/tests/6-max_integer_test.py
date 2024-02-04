06c77cd25f98c7996a47d901baf76c4c0a297755
Murad Abbaszade <muradabbaszade6@gmail.com>
2024-02-04 11:01:26 -0800

    addd 6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import unittest
+
+max_integer = __import__("6-max_integer").max_integer
+
+class TestMaxInteger(unittest.TestCase):
+        
+    def test1(self):
+        self.assertIsNone(max_integer([]))
+
+    def test2(self):
+        self.assertEqual(max_integer([-1, -3, -5, -4]), -1)
+        self.assertEqual(max_integer([-4, -3, -5, -1]), -1)
+        
+    def test3(self):
+        self.assertEqual(max_integer([1, -2, 3, 4]), 4)
+        self.assertEqual(max_integer([5, 2, -3, 4]), 5)
+
+    def test4(self):
+        self.assertEqual(max_integer([1]), 1)
