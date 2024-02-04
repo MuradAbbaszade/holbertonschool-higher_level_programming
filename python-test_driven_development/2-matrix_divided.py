aa14ec9e44b57ae0abaa9de3603af32f687d89b0
Murad Abbaszade <muradabbaszade6@gmail.com>
2024-02-04 09:57:07 -0800

    Add 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    for i in matrix:
+        for j in i:
+            if not (isinstance(j, int) or isinstance(j, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    k = len(matrix[0])
+    for i in matrix:
+        if len(i) != k:
+            raise TypeError("Each row of the matrix must have the same size")
+    if not (isinstance(div, int) or isinstance(div, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    for i in matrix:
+        new_row = []
+        for j in i:
+            new_row.append(round(j / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix
+        
