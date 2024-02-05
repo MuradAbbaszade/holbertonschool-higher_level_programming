d3dae7e6060c1bb53c01d7d0383b319251e334fa
Murad Abbaszade <muradabbaszade6@gmail.com>
2024-02-05 03:45:26 -0800

    Add 2

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Init"""
+        self.width = width
+        self.height = height
+
+    @property
+    def widht(self):
+        """Get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Set width"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Set height"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+    
