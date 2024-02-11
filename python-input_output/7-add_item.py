ea8095a894329432afd8ea8e2ff022604f5088c5
Murad Abbaszade <muradabbaszade6@gmail.com>
2024-02-11 06:56:20 -0800

    Add 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Load add save module"""
+import json
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+arg_list = []
+for i in sys.argv:
+    arg_list.append(i)
+del arg_list[0]
+save_to_json_file(arg_list, "add_item.json")
+    
