9eb67803f49cf2ccd978cd2b26d00f14c2f622ab
Murad Abbaszade <muradabbaszade6@gmail.com>
2024-02-11 08:08:39 -0800

    Add 1

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -13,10 +13,16 @@ class Student:
     def to_json(self, attrs = None):
         """To json function"""
         result_dict = {}
-        if not (attrs is None)
+        if not (attrs is None):
             for i in attrs:
                 if i in self.__dict__:
                     result_dict[i] = self.__dict__[i]
             return result_dict
         else:
             return self.__dict__
+
+    def reload_from_json(self, json):
+        """Reload from json function"""
+        self.first_name = json[first_name]
+        self.last_name = json[last_name]
+        self.age = json[age]
