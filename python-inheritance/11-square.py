d42c8dadaedfc6657c75c2d8ddec966f8ea0adf3
Murad Abbaszade <muradabbaszade6@gmail.com>
2024-02-11 04:53:42 -0800

    Add 1

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square module"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        """Init function"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Area function"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Str function"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
