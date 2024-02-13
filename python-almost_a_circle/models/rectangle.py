7d2d977659c1f4ff30680ff694a741e7f355f3fa
MuradAbbaszade <muradabbaszade6@gmail.com>
2024-02-13 03:30:25 -0800

    Add area function

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -60,3 +60,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """Area function"""
+        return self.__height * self.__width
