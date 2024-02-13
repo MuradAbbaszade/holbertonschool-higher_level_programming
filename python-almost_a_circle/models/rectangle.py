b722cb9e098f9690cd43f202cb3dc1ac7358a513
MuradAbbaszade <muradabbaszade6@gmail.com>
2024-02-13 04:01:36 -0800

    Update str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,7 +73,8 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
+                                                       self.y, self.width, self.height)
 
     def update(self, *args):
         """Update function"""
