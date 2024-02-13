72e25f26c8e346edc4c2c85e42ebaa556c898b74
MuradAbbaszade <muradabbaszade6@gmail.com>
2024-02-13 04:28:16 -0800

    Add getter and setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """Square module"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
@@ -13,3 +14,12 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y,
                                                  self.__size)
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
+        self.__size = size
