109fdd044f7f64cbdf7afab213f0929372c13221
MuradAbbaszade <muradabbaszade6@gmail.com>
2024-02-18 08:51:01 -0800

    Add 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,3 +35,9 @@ class Base:
             filename = "{}.json".format(i.__class__.__name__)
             with open(filename, "w") as file:
                 file.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or bool(json_string) is False:
+            return []
+        return json.loads(json_string)
