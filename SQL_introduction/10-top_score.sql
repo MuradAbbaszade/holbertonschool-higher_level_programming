89b45a08fee0680f59dcff6ed791ef786cabd0ae
muradabbaszade6@gmail.com <muradabbaszade6@gmail.com>
2024-03-23 15:12:35 +0000

    update

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- 10
-SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT score, name FROM `second_table` ORDER BY score DESC;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- 8
-SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT COUNT(*) FROM `first_table` WHERE id = 89;
